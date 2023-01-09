eaf42c0bb4084987bd8df4f1bd7c96bd442d89d2
alanionita <105461667+alanionita@users.noreply.github.com>
2023-01-09 23:45:50 +0000

    FEAT : adds read command
    Added a read command with a simple flag default hardcoded
    
    Read command opens the file
    
    Then logs the frontmatter metadata - will be useful in later stages
    
    Commented out example commands (Docker example)

---
+++ commands/read.py
@@ -0,0 +1,34 @@
+import click
+from datetime import datetime
+from utils import frontmatter
+import os
+
+import os
+def open_note_in_vscode(file_name):
+    os.system("code " + file_name)
+
+def read_note(file_name):
+    CURR_WORK_DIR = os.getcwd() 
+    NOTES_FILE_PATH = f'{CURR_WORK_DIR}/my_notes' 
+    NOTES_FILE_EXT = '.md'
+    note_path = f'{NOTES_FILE_PATH}/{file_name}{NOTES_FILE_EXT}'
+    try:
+        note_frontmatter = frontmatter.make(f'{file_name}')
+        with open(note_path) as f:
+            # Adds debugging for frontmatter metadata
+            frontmatter.print_metadata(f)
+            pass
+
+    except FileExistsError as err: 
+        # if file exists I should just open the file
+        print(f'Error [create_note] : Note [{title}] already exists, opening note now')
+        open_note_in_vscode(note_path)
+
+    except BaseException as err:
+        print(f"Error [create_note] : Unexpected {err=}, {type(err)=}")
+
+@click.command()
+@click.option('--name', '-n', default="daily.2023.01.09", prompt='Enter type of note to read', help='Name of note')
+def cmd_read(name):
+    print(f'Reading note of type : {name}')
+    read_note(name)
--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import click
 from commands import build
 from commands import deploy
 from commands import create
+from commands import read
 
 @click.group(help="CLI tool to manage my notes garden")
 def cli():
     pass
 
-cli.add_command(build.build)
-cli.add_command(deploy.deploy)
+# cli.add_command(build.build)
+# cli.add_command(deploy.deploy)
 cli.add_command(create.cmd_create, name='create')
+cli.add_command(read.cmd_read, name='read')
 
 
 if __name__ == '__main__':
