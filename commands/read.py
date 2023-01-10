94101dffd1c1c9e835a60611c967be07b7b8ae9f
alanionita <105461667+alanionita@users.noreply.github.com>
2023-01-10 23:46:44 +0000

    FEAT : implemented the open in vscode in the read note

--- commands/read.py
+++ commands/read.py
@@ -17,15 +17,12 @@ def read_note(file_name):
         with open(note_path) as f:
             # Adds debugging for frontmatter metadata
             frontmatter.print_metadata(f)
+            # Open in vscode
+            open_note_in_vscode(note_path)
             pass
 
-    except FileExistsError as err: 
-        # if file exists I should just open the file
-        print(f'Error [create_note] : Note [{title}] already exists, opening note now')
-        open_note_in_vscode(note_path)
-
     except BaseException as err:
-        print(f"Error [create_note] : Unexpected {err=}, {type(err)=}")
+        print(f"Error [read_note] : Unexpected {err=}, {type(err)=}")
 
 @click.command()
 @click.option('--name', '-n', default="daily.2023.01.09", prompt='Enter type of note to read', help='Name of note')
