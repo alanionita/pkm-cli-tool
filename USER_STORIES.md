b531191a3a894fdc9dcf8e407067e94907149aea
alanionita <105461667+alanionita@users.noreply.github.com>
2023-01-11 23:11:27 +0000

    FEAT (create) : adds ability to create custom project, area, and resource notes

---
+++ USER_STORIES.md
@@ -0,0 +1,13 @@
+# User Stories
+
+As a note-taking person:
+    - I want to be able to create new daily notes                               [done]
+    - I want to open notes in 
+        - VSCode                                                                [done]
+        - other text editors
+    - when I open notes I want the `updated` frontmatter to be updated
+    - I want to be able to create new project notes                             [done]
+    - I want to be able to create new area notes                                [done]
+    - I want to be able to create new resources notes                           [done]
+    - I want to be able to archive notes
+
--- commands/create.py
+++ commands/create.py
@@ -2,10 +2,11 @@ import click
 from datetime import datetime
 from utils import frontmatter, make_note_path, open_note_in_vscode
 
+
 def create_note(filename, file_contents, title):
     note_path = make_note_path(filename)
     try:
-        note_frontmatter = frontmatter.make(title)
+        note_frontmatter = frontmatter.make(f'{title}')
         with open(note_path, 'x') as fp:
             fp.writelines(note_frontmatter)
             fp.write(file_contents)
@@ -13,58 +14,54 @@ def create_note(filename, file_contents, title):
             open_note_in_vscode(note_path)
             pass
 
-    except FileExistsError as err: 
+    except FileExistsError as err:
         # if file exists I should just open the file
-        print(f'Error [create_note] : Note [{title}] already exists, opening note now')
+        print(
+            f'Error [create_note] : Note [{title}] already exists, opening note now')
         open_note_in_vscode(note_path)
 
     except BaseException as err:
         print(f"Error [create_note] : Unexpected {err=}, {type(err)=}")
 
+
 def make_note_daily():
-    print(f'Creating daily note')
     curr_date = datetime.now()
     note_path = curr_date.strftime('daily.%Y.%m.%d')
     note_title = curr_date.strftime('%Y-%m-%d')
-    note_contents = 'test'
+    note_contents = ''
     create_note(note_path, note_contents, note_title)
     return
 
-def make_note_project(): 
-    print(f'Creating project note')
+
+def make_note_other(type, name):
+    
+    path_arr = name.split('.')
+    path_last_part = path_arr[-1]
+
+    curr_date = datetime.now()
+    note_path = curr_date.strftime(f'{type}.{name}')
+    note_title = path_last_part
+    note_contents = ''
+    create_note(note_path, note_contents, note_title)
     return
 
-def make_note_area(): 
-    print(f'Creating area note')
-    return
 
-def make_note_resource(): 
-    print(f'Creating resource note')
-    return
-
-def make_note_archive(): 
-    print(f'Creating archive note')
-    return
-
-note_types={
-    'daily': make_note_daily,
-    'project': make_note_project,
-    'area': make_note_area,
-    'resource': make_note_resource,
-    'archive': make_note_archive
-}
-
-def make_note(type):
-
-    # return switch.get(type,"Invalid note type")
-    make_note = note_types[type]
-    if callable(make_note):
-        make_note()
+def make_note(type, name):
+    if (type == 'daily'):
+        make_note_daily()
     else:
-        print(make_note)
+        make_note_other(type, name)
+
+
+cmd_note_types = ['daily', 'project', 'area', 'resource', 'archive'] # Excludes 'daily'
 
 @click.command()
-@click.option('--type', '-t', default="daily", type=click.Choice(['daily', 'project', 'area', 'resource', 'archive'], case_sensitive=False), prompt='Enter type of note to create', help='Type of note')
-def cmd_create(type):
-    print(f'Creating note of type : {type}')
-    make_note(type)
+@click.option('--type', '-t', default="daily", type=click.Choice(cmd_note_types, case_sensitive=False), prompt='Enter type of note to create', help='Type of note')
+@click.argument("name", required=False)
+
+def cmd_create(type, name):
+    print(f'CMD: Creating note of type ::: {type}')
+
+    if (type != 'daily'):
+        make_note(type, name)
+
