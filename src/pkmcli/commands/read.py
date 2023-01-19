956f56aba206f9ac3701bb63ebb15a7e48ad7ff7
alanionita <105461667+alanionita@users.noreply.github.com>
2023-01-19 22:34:10 +0000

    FEAT (cmd/read): adds notes_store context to the read command
    Also updates the path generator to make it more dynamic

--- src/pkmcli/commands/read.py
+++ src/pkmcli/commands/read.py
@@ -1,8 +1,9 @@
 import click
 from pkmcli.generators import fm, path
+from pkmcli.generators.store import pass_store
 
-def read_note(filename):
-    note_path = path.make(filename)
+def read_note(notes_store, filename):
+    note_path = path.make(notes_store, filename)
     try:
         with open(note_path) as f:
             """
@@ -21,6 +22,8 @@ def read_note(filename):
 @click.command()
 @click.option('--name', '-n', default="daily.2023.01.15", prompt='Enter type of note to read', help='Name of note')
 
-def cmd(name):
+@pass_store
+def cmd(notes_store, name):
     click.echo(f'Reading note of type : {name}')
-    read_note(name)
+    notes_location = notes_store.location 
+    read_note(notes_location, name)
--- src/pkmcli/generators/path.py
+++ src/pkmcli/generators/path.py
@@ -1,10 +1,15 @@
 import os
 
-def make(filename):
-    CURR_WORK_DIR = os.getcwd() 
-    NOTES_FILE_PATH = f'{CURR_WORK_DIR}/notes_store' 
+def make(notes_store, filename):
     NOTES_FILE_EXT = '.md'
-    path_to_file = f'{NOTES_FILE_PATH}/{filename}{NOTES_FILE_EXT}'
+    path_base = ''
+    if (notes_store):
+        path_base = notes_store
+    else:
+        path_cwd = os.getcwd()
+        path_base = f'{path_cwd}/notes_store'
+
+    path_to_file = f'{path_base}/{filename}{NOTES_FILE_EXT}'
     return path_to_file
 
 def open_in(filename, program):
