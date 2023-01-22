85f2ac45b1ede1240234809d66509b9190c018da
alanionita <105461667+alanionita@users.noreply.github.com>
2023-01-22 00:54:17 +0000

    FIX (store) : refactors the store to define getters and setters using annotations

--- src/pkmcli/generators/store.py
+++ src/pkmcli/generators/store.py
@@ -2,9 +2,29 @@ import click
 import os
 
 class NotesStore(object):
-    def __init__(self, location=None):
-        CURR_WORK_DIR = os.getcwd() 
-        NOTES_STORE_DEFAULT = f'{CURR_WORK_DIR}/notes_store' 
-        self.location = os.path.abspath(location or NOTES_STORE_DEFAULT)
+    # _instance = None
 
-pass_store = click.make_pass_decorator(NotesStore)
+    # def __new__(cls):
+    #     if cls._instance is None:
+    #         cls._instance = super().__new__(cls)
+    #     return cls._instance
+
+    def __init__(self):
+        CURR_WORK_DIR = os.getcwd()
+        NOTES_STORE_DEFAULT = f'{CURR_WORK_DIR}/notes_store'
+        self._location = os.path.abspath(NOTES_STORE_DEFAULT)
+
+    @property
+    def location(self):
+        return self._location
+
+    @location.setter
+    def location(self, location: str):
+        self._location = location
+
+
+pass_store_ctx = click.make_pass_decorator(NotesStore, ensure=True)
+
+def click_get_ctx_location():
+    ctx = click.get_current_context()
+    return ctx.obj.location
