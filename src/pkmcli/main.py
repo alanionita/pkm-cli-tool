be01853bd11cabf67d9a69b400b71023051815c0
alanionita <105461667+alanionita@users.noreply.github.com>
2023-01-22 00:57:40 +0000

    FEAT (store) : created the store group and linked it to the cli app
    Also did a lot of exploration with the context sharing across the app
    
    BUG: context is not working if you run `store status` -> `store change` -> `store status`
    
    The final store status commands returns the original context, not the changed value

--- src/pkmcli/main.py
+++ src/pkmcli/main.py
@@ -1,22 +1,28 @@
 import click
-from pkmcli.generators.store import NotesStore
 from pkmcli.commands import create
 from pkmcli.commands import read
-from pkmcli.commands import echo_store
+from pkmcli.commands.store import status
+from pkmcli.commands.store import change
+from pkmcli.generators.store import NotesStore
 
 @click.group(help="CLI tool to manage my notes garden")
+def cli():
+    ctx = click.get_current_context()
+    if ctx.obj is None:
+        ctx.obj = NotesStore()
+    else:
+        print(f'cli / ctx :::', ctx.obj.location)  
+
+@cli.group()
+def store():
+    pass;
 
 
-@click.option('--notes-home', envvar='PKMCLI_NOTES_HOME')
-@click.pass_context
-def cli(ctx, notes_home):
-    ctx.obj = NotesStore(notes_home)
-    pass
-
-cli.add_command(echo_store.cmd, name='echo-store')
 cli.add_command(create.cmd, name='create')
 cli.add_command(read.cmd, name='read')
 
+store.add_command(change.cmd, name='change')
+store.add_command(status.cmd, name='status')
 
 if __name__ == '__main__':
     cli()
