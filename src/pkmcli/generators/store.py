dd6a85eefb9b693ae78c9d6af04a677107aa0cb4
alanionita <105461667+alanionita@users.noreply.github.com>
2023-01-23 03:18:14 +0000

    FEAT (generators/store) : adds new store api that reads from a json file
    Also adds a new entry to .gitignore: test_store another testing location

--- src/pkmcli/generators/store.py
+++ src/pkmcli/generators/store.py
@@ -1,30 +1,38 @@
 import click
+import json
 import os
 
-class NotesStore(object):
-    # _instance = None
+NOTES_LOCATION = 'notes_location'
 
-    # def __new__(cls):
-    #     if cls._instance is None:
-    #         cls._instance = super().__new__(cls)
-    #     return cls._instance
-
-    def __init__(self):
-        CURR_WORK_DIR = os.getcwd()
-        NOTES_STORE_DEFAULT = f'{CURR_WORK_DIR}/notes_store'
-        self._location = os.path.abspath(NOTES_STORE_DEFAULT)
-
-    @property
-    def location(self):
-        return self._location
-
-    @location.setter
-    def location(self, location: str):
-        self._location = location
+def build_context_path():
+    CURR_WORK_DIR = os.getcwd()
+    CONTEXT_PATH = f'{CURR_WORK_DIR}/context.json'
+    return os.path.abspath(CONTEXT_PATH)
 
 
-pass_store_ctx = click.make_pass_decorator(NotesStore, ensure=True)
+def get_location():
+    ctx_path = build_context_path()
+    try:
+        with open(ctx_path, "r") as f:
+            context = json.load(f)
+            value = context.get(NOTES_LOCATION)
+            if value:
+                return value
+            else:
+                raise KeyError
+    except KeyError:
+        click.echo(f"[get_location] {NOTES_LOCATION} is not set.")
 
-def click_get_ctx_location():
-    ctx = click.get_current_context()
-    return ctx.obj.location
+
+def set_location(new_location):
+    ctx_path = build_context_path()
+    print(f'[set-loc] ctx ::', ctx_path)
+    context = {}
+    try:
+        with open(ctx_path, "r") as f:
+            context = json.load(f)
+    except:
+        pass
+    context[NOTES_LOCATION] = new_location
+    with open(ctx_path, "w") as f:
+        json.dump(context, f)
