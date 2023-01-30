51421515e4f6bca0a025d3628d6edcb63f991536
alanionita <105461667+alanionita@users.noreply.github.com>
2023-01-30 02:14:50 +0000

    REFACTOR (commands) : implements new path and store method changes for store / chang cmd
    Refactored the generators / store / set_location to use the new build_context_path signature
    
    Refactored the commands / store / change to use the new build_context_path api

--- src/pkmcli/commands/store/change.py
+++ src/pkmcli/commands/store/change.py
@@ -3,17 +3,11 @@ import os
 from pkmcli.commands.store import status
 from pkmcli.generators.store import get_location, set_location, build_context_path
 
-CURR_WORK_DIR = os.getcwd()
-NOTES_STORE_DEFAULT = f'{CURR_WORK_DIR}/test_store'
-
-
 @click.command()
 @click.option("--location", required=True)
 def cmd(location):
-    cwd = os.getcwd()
-    base_path = f'{cwd}/src/pkmcli'
-    ctx_path = build_context_path(base_path)
+    ctx_path = build_context_path()
     ctx_location = get_location(ctx_path)
     print('[changelocation] Notes Store / old location ::', ctx_location)
-    set_location(base_path, location)
+    set_location(location)
     click.get_current_context().invoke(status.cmd)
