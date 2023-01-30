30b743e2a87541f66ac80e212e028696f93ff98f
alanionita <105461667+alanionita@users.noreply.github.com>
2023-01-30 23:10:13 +0000

    FEAT (tests) : adds test for generators / path / make()

--- tests/generators_path_test.py
+++ tests/generators_path_test.py
@@ -1,11 +1,24 @@
 import os, pytest
-from pkmcli.generators.path import get_project_base
+from pkmcli.generators.path import get_project_base, make
 
+cwd = os.getcwd()   
 def test_get_project_base():
-    cwd = os.getcwd()
     outcome = get_project_base()
     expected = f'{cwd}/src/pkmcli'
 
     assert expected == outcome, 'Should return the correct project base'
 
     assert outcome.__contains__('pkmcli') == True, 'Base project base should include the project name'
+
+def test_make():
+    test_base_path = cwd
+    test_filename = 'test'
+    test_filename_ext = 'md'
+    expected = f'{test_base_path}/{test_filename}.{test_filename_ext}'
+    outcome = make(test_base_path, test_filename)
+
+    assert expected == outcome, 'Should make the correct path to the file.'
+
+    assert outcome.__contains__(f'.{test_filename_ext}') == True, 'Path to file should include correct extension.'
+
+    assert outcome.__contains__(test_filename) == True, 'Path to file should include correct filename.'
