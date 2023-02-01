eadaf376786b36f964fc168f98acc4fc0273faa4
alanionita <105461667+alanionita@users.noreply.github.com>
2023-02-01 21:12:46 +0000

    FEAT (tests) : updates the command_create tests to run cleanup before the tests

--- tests/commands_create_test.py
+++ tests/commands_create_test.py
@@ -13,28 +13,27 @@ mock_project_name = 'live-test'
 
 def get_store():
     store_status_res = runner.invoke(main.cli, ['store', 'status'])
-    store_path = store_status_res.output.split('\n')[0]
+    store_path = store_status_res.output.split('\n')[-2]
     return store_path
 
-with unittest.mock.patch('datetime.datetime', mock_datetime):
-    # TODO: implement this properly to stop having to remove files before running the tests
-    # @pytest.fixture(scope="session", autouse=True)
-    # def before_all():
-    #     store_path = get_store()
-    #     print(f'store_path :: {store_path}')
-    #     test_project_path = f'{store_path}/project.{mock_project_name}.md'
-    #     os.remove(test_project_path)
-    #     curr_date = datetime.datetime.now()
-    #     daily_fname = curr_date.strftime('daily.%Y.%m.%d')
-    #     test_daily_path = f'{store_path}/{daily_fname}.md'
-    #     os.remove(test_daily_path)
+@pytest.fixture(scope="session", autouse=True)
+def before_all():
+    store_path = get_store()
+    test_project_path = f'{store_path}/project.{mock_project_name}.md'
+    if (os.path.exists(test_project_path)):
+        os.remove(test_project_path)
+    with unittest.mock.patch('datetime.datetime', mock_datetime):
+        curr_date = datetime.datetime.now()
+        daily_fname = curr_date.strftime('daily.%Y.%m.%d')
+        test_daily_path = f'{store_path}/{daily_fname}.md'
+        if (os.path.exists(test_daily_path)):
+            os.remove(test_daily_path)
 
-
-    def test_create_daily():
-        with unittest.mock.patch('datetime.datetime', mock_datetime):
-            result = runner.invoke(main.cli, ['create', '--type', 'daily'])
-            assert result.exit_code == 0
-
-    def test_create_project():
-        result = runner.invoke(main.cli, ['create', '--type', 'project', mock_project_name])
+def test_create_daily():
+    with unittest.mock.patch('datetime.datetime', mock_datetime):
+        result = runner.invoke(main.cli, ['create', '--type', 'daily'])
         assert result.exit_code == 0
+
+def test_create_project():
+    result = runner.invoke(main.cli, ['create', '--type', 'project', mock_project_name])
+    assert result.exit_code == 0
